dc68cfc1e8dff10fc5cc4ddb464b9ee4a38f4922
Petter Karlsson <petter.karlsson@cmeducations.se>
2022-09-01 11:35:02 +0200

    added postgres url

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Client } = require("pg");
 
 const db_url =
   process.env.DATABASE_URL ||
-  "postgres://uidkhwvqmqdaxl:2114fa3a41d6e319ac9d7c3a749f6e1f8df4ea661b441885e5990ce7119d9d7b@ec2-54-246-185-161.eu-west-1.compute.amazonaws.com:5432/d37rovpofqgf0b";
+  "postgres://ckvmwvwcywojff:deb51638a11cd98b3806986c29c33dad82a5cd3c71497d9b698285fd9fe16dad@ec2-54-75-26-218.eu-west-1.compute.amazonaws.com:5432/d7nl1gfl5uifv1";
 
 const client = new Client({
   connectionString: db_url,
